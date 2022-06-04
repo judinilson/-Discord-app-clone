783d24bf1685c8a0892d9b9830f1c494d73ae3db
judinilson monchacha <judinilson0000@gmail.com>
2022-06-04 22:51:23 +0400

    create channel

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -5,9 +6,30 @@ import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
 
+import { StreamChat } from "stream-chat";
+import { useEffect } from "react";
+
+const API_KEY = "kz2qdnrtyh4e";
+const client = StreamChat.getInstance(API_KEY);
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const connectUser = async () => {
+    // sign in user with backend and get the user token
+    await client.connectUser(
+      {
+        id: "juarmo",
+        name: "juarmo",
+        imageUrl: "https://",
+      },
+      client.devToken("juarmo")
+    );
+  };
+  useEffect(() => {
+    connectUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
